1f03c8af7d3a14282c9f5a1e0ce97355cd9f9a70
Lyubomyr <lubomyr31@gmail.com>
2018-05-02 22:49:58 +0300

    implemented databinding support

--- app/src/main/java/com/testtaskapp/MainActivity.java
+++ app/src/main/java/com/testtaskapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.testtaskapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.testtaskapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
