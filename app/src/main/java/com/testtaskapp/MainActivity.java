007908dab32c56c9e2f97d702066a5c3ef70d3ef
Lyubomyr <lubomyr31@gmail.com>
2018-05-03 21:46:48 +0300

    implementation of bottom navigation menu, adding images

--- app/src/main/java/com/testtaskapp/MainActivity.java
+++ app/src/main/java/com/testtaskapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.testtaskapp;
 
 import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.testtaskapp.databinding.ActivityMainBinding;
 
@@ -13,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        bindingBottomMenu();
+    }
+
+    private void bindingBottomMenu() {
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.item_audiobooks:
+                                break;
+                            case R.id.item_movies:
+                                break;
+                            case R.id.item_podcasts:
+                                break;
+                            case R.id.item_favorites:
+                                break;
+                        }
+                        return true;
+                    }
+                });
     }
 }
