ca1852f6d620244d49f7643198d8aa0756402a1d
Lyubomyr <lubomyr31@gmail.com>
2018-05-03 22:24:58 +0300

    entities implementation and local database support

---
+++ app/src/main/java/com/testtaskapp/BaseApplication.java
@@ -0,0 +1,25 @@
+package com.testtaskapp;
+
+import android.app.Application;
+
+import com.testtaskapp.entities.DaoMaster;
+import com.testtaskapp.entities.DaoSession;
+
+import org.greenrobot.greendao.database.Database;
+
+public class BaseApplication extends Application {
+    private static DaoSession daoSession;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "app-db");
+        Database db = helper.getWritableDb();
+        daoSession = new DaoMaster(db).newSession();
+    }
+
+    public static DaoSession getDaoSession() {
+        return daoSession;
+    }
+}
---
+++ app/src/main/java/com/testtaskapp/entities/FeedItem.java
@@ -0,0 +1,112 @@
+package com.testtaskapp.entities;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+@Entity
+public class FeedItem {
+    @Id
+    @Expose
+    @SerializedName("id")
+    private long id;
+
+    @Expose
+    @SerializedName("artistName")
+    private String artistName;
+
+    @Expose
+    @SerializedName("name")
+    private String name;
+
+    @Expose
+    @SerializedName("kind")
+    private String kind;
+
+    @Expose
+    @SerializedName("artistId")
+    private String artistId;
+
+    @Expose
+    @SerializedName("artworkUrl100")
+    private String artworkUrl100;
+
+    @Expose
+    @SerializedName("url")
+    private String url;
+
+    @Generated(hash = 51825878)
+    public FeedItem(long id, String artistName, String name, String kind,
+            String artistId, String artworkUrl100, String url) {
+        this.id = id;
+        this.artistName = artistName;
+        this.name = name;
+        this.kind = kind;
+        this.artistId = artistId;
+        this.artworkUrl100 = artworkUrl100;
+        this.url = url;
+    }
+
+    @Generated(hash = 605716718)
+    public FeedItem() {
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getArtistName() {
+        return this.artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKind() {
+        return this.kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getArtistId() {
+        return this.artistId;
+    }
+
+    public void setArtistId(String artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getArtworkUrl100() {
+        return this.artworkUrl100;
+    }
+
+    public void setArtworkUrl100(String artworkUrl100) {
+        this.artworkUrl100 = artworkUrl100;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
